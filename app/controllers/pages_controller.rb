c77d3e8253a971471af6c925641b17ed320519e0
Susumu <sumura80@gmail.com>
2019-01-05 14:23:52 +0900

    finished terms of service

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def about
   	render "pages/about"
   end
+
+  def terms
+  	render "pages/terms"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'about' ,to:'pages#about'
+  get 'terms' , to:'pages#terms'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
